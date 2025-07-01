9cf8cf8d9df5e22f2929032562d74fa37cb197cb
Stein Welberg <mail@steinwelberg.nl>
2025-07-01 12:20:29 +0200

    chore: use vite React SSG to statically render

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,9 @@
-import React from "react";
-import ReactDOM, { hydrateRoot } from "react-dom/client";
 import App from "./App";
 import { init, trackPages } from "insights-js";
 import "./index.css";
+import { ViteReactSSG } from "vite-react-ssg/single-page";
 
-const rootElement = document.getElementById("root") as HTMLElement;
-const root = ReactDOM.createRoot(rootElement);
 init("hKyFepFG_w7OWnw6");
 trackPages();
 
-if (rootElement.hasChildNodes()) {
-  hydrateRoot(rootElement, <App />);
-} else {
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
-}
+export const createRoot = ViteReactSSG(<App />);
